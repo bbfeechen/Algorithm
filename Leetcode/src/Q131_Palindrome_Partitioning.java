8b13955ee3441f90a217da00e6a631fac3a31543
Kailiang Chen <bbfeechen@gmail.com>
2015-03-22 03:47:21 -0400

    update

---
+++ Leetcode/src/Q131_Palindrome_Partitioning.java
@@ -0,0 +1,52 @@
+import java.util.ArrayList;
+
+
+public class Q131_Palindrome_Partitioning {
+	public static ArrayList<ArrayList<String>> partition(String s) {
+        ArrayList<ArrayList<String>> result = new ArrayList<ArrayList<String>>();
+        if(s == null || s.length() == 0) {
+            return result;
+        }
+        ArrayList<String> solution = new ArrayList<String>();
+        helper(result, solution, s, 0);
+        return result;
+    }
+    
+    private static void helper(ArrayList<ArrayList<String>> result, ArrayList<String> solution, String s, int index) {
+        if(index == s.length()) {
+            result.add(new ArrayList<String>(solution));
+            return;
+        }
+        
+        for(int i = index; i < s.length(); i++) {
+            String str = s.substring(index, i + 1);
+            if(!isPalindrome(str)) {
+                continue;
+            }
+            solution.add(str);
+            helper(result, solution, s, i + 1);
+            solution.remove(solution.size() - 1);
+        }
+    }
+    
+    private static boolean isPalindrome(String str) {
+        if(str == null) {
+            return true;
+        }
+        int start = 0, end = str.length() - 1;
+        while(start < end) {
+            if(str.charAt(start) != str.charAt(end)) {
+                return false;
+            } 
+            start++;
+            end--;
+        }
+        return true;
+    }
+    
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Leetcode/src/Q135_Candy.java
@@ -0,0 +1,37 @@
+import java.util.Arrays;
+
+
+public class Q135_Candy {
+	public static int candy(int[] ratings) {
+        if(ratings == null || ratings.length == 0) {
+            return 0;
+        }
+        
+        int[] dp = new int[ratings.length];
+        Arrays.fill(dp, 1);
+        
+        for(int i = 1; i < dp.length; i++) {
+            if(ratings[i] > ratings[i - 1]) {
+                dp[i] = dp[i - 1] + 1;
+            }
+        }
+        
+        for(int i = dp.length - 2; i >= 0; i--) {
+            if(ratings[i] > ratings[i + 1] && dp[i] <= dp[i + 1]) {
+                dp[i] = dp[i + 1] + 1;
+            }
+        }
+        
+        int sum = 0;
+        for(int i = 0; i < dp.length; i++) {
+            sum += dp[i];
+        }
+        return sum;
+    }
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
