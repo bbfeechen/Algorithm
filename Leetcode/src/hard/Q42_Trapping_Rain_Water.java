c9c9acd9ab38098c03ebfca358a5a2761ad64177
Kailiang Chen <bbfeechen@gmail.com>
2015-08-06 22:15:20 -0700

    update

--- Leetcode/src/hard/Q42_Trapping_Rain_Water.java
+++ Leetcode/src/hard/Q42_Trapping_Rain_Water.java
@@ -31,8 +31,8 @@ public class Q42_Trapping_Rain_Water {
     }
 	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int[] height = {0,1,0,2,1,0,1,3,2,1,2,1};
+		System.out.println(trap(height));
 	}
 
 }
