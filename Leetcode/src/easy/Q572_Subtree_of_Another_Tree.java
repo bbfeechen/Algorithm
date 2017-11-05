c7905253b6a4cf9259360b1d071d8d39104e94ff
Kailiang Chen <kailianc@uber.com>
2017-11-04 21:44:38 -0700

    Add Q572

---
+++ Leetcode/src/easy/Q572_Subtree_of_Another_Tree.java
@@ -0,0 +1,50 @@
+package easy;
+
+public class Q572_Subtree_of_Another_Tree {
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        public TreeNode(int val) {
+            this.val = val;
+        }
+    }
+
+    public static boolean isSubtree(TreeNode s, TreeNode t) {
+        if (s == null) {
+            return false;
+        }
+        if (isSame(s, t)) {
+            return true;
+        }
+        return isSubtree(s.left, t) || isSubtree(s.right, t);
+    }
+
+    private static boolean isSame(TreeNode s, TreeNode t) {
+        if (s == null && t == null) {
+            return true;
+        }
+        if (s == null || t == null) {
+            return false;
+        }
+        if (s.val != t.val) {
+            return false;
+        }
+        return isSame(s.left, t.left) && isSame(s.right, t.right);
+    }
+
+
+    public static void main(String[] args) {
+        TreeNode root1 = new TreeNode(3);
+        root1.left = new TreeNode(4);
+        root1.right = new TreeNode(5);
+        root1.left.left = new TreeNode(1);
+        root1.left.right = new TreeNode(2);
+
+        TreeNode root2 = new TreeNode(4);
+        root2.left = new TreeNode(1);
+        root2.right = new TreeNode(2);
+
+        System.out.println(isSubtree(root1, root2));
+    }
+}
