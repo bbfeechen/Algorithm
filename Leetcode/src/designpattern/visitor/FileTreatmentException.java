6013474820fb9d49df2a48149d1edab2c57ae02e
Kailiang Chen <kailianc@uber.com>
2017-11-23 16:52:51 -0800

    Add Visitor Pattern

---
+++ Leetcode/src/designpattern/visitor/FileTreatmentException.java
@@ -0,0 +1,11 @@
+package designpattern.visitor;
+
+public class FileTreatmentException extends RuntimeException {
+
+    public FileTreatmentException() {
+    }
+
+    public FileTreatmentException(String message) {
+        super(message);
+    }
+}
---
+++ Leetcode/src/designpattern/visitor/Visitor.java
@@ -0,0 +1,7 @@
+package designpattern.visitor;
+
+public abstract class Visitor {
+    public abstract void visit(File file);
+
+    public abstract void visit(Directory directory);
+}
