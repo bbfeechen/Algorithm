9a1f5fc8f01809947618cefa98023c4aec281300
Kailiang Chen <kailianc@uber.com>
2017-11-23 23:55:43 -0800

    Add Observer Pattern

---
+++ Leetcode/src/designpattern/observer/DigitObserver.java
@@ -0,0 +1,14 @@
+package designpattern.observer;
+
+public class DigitObserver implements Observer {
+
+    @Override
+    public void update(NumberGenerator generator) {
+        System.out.println("DigitObserver:" + generator.getNumber());
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
