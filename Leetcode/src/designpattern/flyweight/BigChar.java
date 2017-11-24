3a86b204bf7727c8e51ca3849ebce6d11341c7dd
Kailiang Chen <kailianc@uber.com>
2017-11-23 23:33:00 -0800

    Add Flyweight Pattern

---
+++ Leetcode/src/designpattern/flyweight/BigChar.java
@@ -0,0 +1,31 @@
+package designpattern.flyweight;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class BigChar {
+    private char charname;
+    private String fontdata;
+
+    public BigChar(char charname) {
+        this.charname = charname;
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader("big" + charname + ".txt"));
+            String line;
+            StringBuffer buf = new StringBuffer();
+            while ((line = reader.readLine()) != null) {
+                buf.append(line);
+                buf.append("\n");
+            }
+            reader.close();
+            this.fontdata = buf.toString();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void print() {
+        System.out.println(fontdata);
+    }
+}
---
+++ Leetcode/src/designpattern/flyweight/BigString.java
@@ -0,0 +1,20 @@
+package designpattern.flyweight;
+
+public class BigString {
+    private BigChar[] bigChars;
+
+    public BigString(String string) {
+        this.bigChars = new BigChar[string.length()];
+        BigCharFactory factory = BigCharFactory.getInstance();
+        for (int i = 0; i < bigChars.length; i++) {
+            bigChars[i] = factory.getBigChar(string.charAt(i));
+        }
+    }
+
+    public void print() {
+        for (int i = 0; i < bigChars.length; i++) {
+            bigChars[i].print();
+        }
+    }
+}
+
