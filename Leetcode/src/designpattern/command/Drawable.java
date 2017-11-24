7fb04457b0218521b28dca7c5d7e9bf9874ddc74
Kailiang Chen <kailianc@uber.com>
2017-11-24 00:34:08 -0800

    Add Command Pattern

---
+++ Leetcode/src/designpattern/command/Drawable.java
@@ -0,0 +1,5 @@
+package designpattern.command;
+
+public interface Drawable {
+    void draw(int x, int y);
+}
---
+++ Leetcode/src/designpattern/command/Main.java
@@ -0,0 +1,98 @@
+package designpattern.command;
+
+import java.awt.HeadlessException;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionListener;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
+
+import javax.swing.Box;
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+
+public class Main extends JFrame implements ActionListener, MouseMotionListener, WindowListener {
+
+    private MacroCommand history = new MacroCommand();
+    private DrawCanvas canvas = new DrawCanvas(400, 400, history);
+    private JButton clearBtn = new JButton("clear");
+
+    public Main(String title) throws HeadlessException {
+        super(title);
+
+        this.addWindowListener(this);
+        canvas.addMouseMotionListener(this);
+        clearBtn.addActionListener(this);
+
+        Box buttonBox = new Box(BoxLayout.X_AXIS);
+        buttonBox.add(clearBtn);
+        Box mainBox = new Box(BoxLayout.Y_AXIS);
+        mainBox.add(buttonBox);
+        mainBox.add(canvas);
+        getContentPane().add(mainBox);
+
+        pack();
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new Main("Command Pattern Sample");
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == clearBtn) {
+            history.clear();
+            canvas.repaint();
+        }
+    }
+
+    @Override
+    public void mouseDragged(MouseEvent e) {
+        Command cmd = new DrawCommand(canvas, e.getPoint());
+        history.append(cmd);
+        cmd.execute();
+    }
+
+    @Override
+    public void mouseMoved(MouseEvent e) {
+
+    }
+
+    @Override
+    public void windowOpened(WindowEvent e) {
+
+    }
+
+    @Override
+    public void windowClosing(WindowEvent e) {
+        System.exit(0);
+    }
+
+    @Override
+    public void windowClosed(WindowEvent e) {
+
+    }
+
+    @Override
+    public void windowIconified(WindowEvent e) {
+
+    }
+
+    @Override
+    public void windowDeiconified(WindowEvent e) {
+
+    }
+
+    @Override
+    public void windowActivated(WindowEvent e) {
+
+    }
+
+    @Override
+    public void windowDeactivated(WindowEvent e) {
+
+    }
+}
